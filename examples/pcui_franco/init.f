ade5f1f0819209eed655cc0cc411d62785aaad0f
Bobby Arthur <barthur@stanford.edu>
2014-05-28 09:57:18 -0700

    added ran num gen to init.f

--- examples/pcui_franco/init.f
+++ examples/pcui_franco/init.f
@@ -262,3 +262,21 @@ ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
       return
       end
 
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+        function ran0(idum)
+
+        integer idum,IA,IM,IQ,IR,MASK
+        double precision ran0,AM
+        parameter(IA=16807,IM=2147483647,AM=1./IM,
+     <       IQ=127773,IR=2836,MASK=123459876)
+        integer k
+
+        idum=ieor(idum,MASK)
+        k=idum/IQ
+        idum=IA*(idum-k*IQ)-IR*k
+        if (idum.lt.0) idum=idum+IM
+        ran0=AM*idum
+        idum=ieor(idum,MASK)
+        return
+        end
